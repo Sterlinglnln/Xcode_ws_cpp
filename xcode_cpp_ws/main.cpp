064c5ea8bbd5a8656ecc72bd7e28ac7bb55428c2
Lin Junbao <fpnf97@outlook.com>
2025-10-01 03:13:39 +0800

    Initial Commit

---
+++ xcode_cpp_ws/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  xcode_cpp_ws
+//
+//  Created by Larry Lin on 2025/10/1.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
